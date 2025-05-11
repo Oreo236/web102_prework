febfe9fb9be326cc28cad4258133073619af3de7
Oreoluwa <oaa42@cornell.edu>
2025-05-11 17:40:03 -0400

    Finished all the stages of the game

--- README.md
+++ README.md
@@ -1,10 +1,10 @@
-# WEB102 Prework - *Name of App Here*
+# WEB102 Prework - *Sea Monster Games*
 
-Submitted by: **Your Name Here**
+Submitted by: **Oreoluwa Adeniyi**
 
-**Name of your app** is a website for the company Sea Monster Crowdfunding that displays information about the games they have funded.
+**Sea Monster Games** is a website for the company Sea Monster Crowdfunding that displays information about the games they have funded.
 
-Time spent: **X** hours spent in total
+Time spent: **4** hours spent in total
 
 ## Required Features
 
@@ -23,22 +23,19 @@ The following **optional** features are implemented:
 
 Here's a walkthrough of implemented features:
 
-<img src='http://i.imgur.com/link/to/your/gif/file.gif' title='Video Walkthrough' width='' alt='Video Walkthrough' />
+<img src='https://www.loom.com/share/c1f488d23e754bdd8f6b0c94d1ddaf77.gif' title='Video Walkthrough' width='' alt='Video Walkthrough' />
 
 <!-- Replace this with whatever GIF tool you used! -->
-GIF created with ...  
-<!-- Recommended tools:
-[Kap](https://getkap.co/) for macOS
-[ScreenToGif](https://www.screentogif.com/) for Windows
-[peek](https://github.com/phw/peek) for Linux. -->
+GIF created with Loom, I am tried using different apps and websites to upload my gif but only Loom worked for me which was why I used it.
 
 ## Notes
 
 Describe any challenges encountered while building the app.
+- I struggled with remembering what functions I typically use for certain things in JavaScript as I have not used JavaScript in a while.
 
 ## License
 
-    Copyright [yyyy] [name of copyright owner]
+    Copyright [2025] [Oreoluwa Adeniyi]
 
     Licensed under the Apache License, Version 2.0 (the "License");
     you may not use this file except in compliance with the License.
--- index.js
+++ index.js
@@ -27,7 +27,22 @@ const gamesContainer = document.getElementById("games-container");
 
 // create a function that adds all data from the games array to the page
 function addGamesToPage(games) {
+    gamesContainer.innerHTML = '';
+    games.forEach(game => {
+        const gameCard = document.createElement('div')
+        gameCard.classList.add('game-card');
 
+        gameCard.innerHTML = `
+        <img src = "${game.img}" alt= "${game.name}" class="game-img">
+        <h3>${game.name}</h3>
+        <p> ${game.description}</p>
+        <p> Plegdged: ${game.pledged}</p>
+        <p> Goal: ${game.goal}</p>
+        <p> Backers: ${game.backers}</p>
+        `;
+
+        gamesContainer.appendChild(gameCard)
+    })
     // loop over each item in the data
 
 
@@ -48,6 +63,7 @@ function addGamesToPage(games) {
 }
 
 // call the function we just defined using the correct variable
+addGamesToPage(GAMES_JSON)
 // later, we'll call this function using a different list of games
 
 
@@ -61,20 +77,23 @@ function addGamesToPage(games) {
 const contributionsCard = document.getElementById("num-contributions");
 
 // use reduce() to count the number of total contributions by summing the backers
-
+const totalContributions = GAMES_JSON.reduce((total, game) => {return total + game.backers;},0);
+contributionsCard.textContent = totalContributions.toLocaleString();
 
 // set the inner HTML using a template literal and toLocaleString to get a number with commas
 
 
 // grab the amount raised card, then use reduce() to find the total amount raised
 const raisedCard = document.getElementById("total-raised");
-
+const totalRaised = GAMES_JSON.reduce((total, game) => {return total + game.pledged;},0);
+raisedCard.textContent = `$${totalRaised.toLocaleString()}`;
 // set inner HTML using template literal
 
 
 // grab number of games card and set its inner HTML
 const gamesCard = document.getElementById("num-games");
-
+const totalGames = GAMES_JSON.length
+gamesCard.textContent = totalGames.toLocaleString();
 
 /*************************************************************************************
  * Challenge 5: Add functions to filter the funded and unfunded games
@@ -85,12 +104,12 @@ const gamesCard = document.getElementById("num-games");
 // show only games that do not yet have enough funding
 function filterUnfundedOnly() {
     deleteChildElements(gamesContainer);
-
+    
     // use filter() to get a list of games that have not yet met their goal
-
+    const unfundedGames = GAMES_JSON.filter(game => game.pledged < game.goal)
 
     // use the function we previously created to add the unfunded games to the DOM
-
+    addGamesToPage(unfundedGames)
 }
 
 // show only games that are fully funded
@@ -98,9 +117,10 @@ function filterFundedOnly() {
     deleteChildElements(gamesContainer);
 
     // use filter() to get a list of games that have met or exceeded their goal
-
+    const fundedGames = GAMES_JSON.filter(game => game.pledged >= game.goal)
 
     // use the function we previously created to add unfunded games to the DOM
+    addGamesToPage(fundedGames)
 
 }
 
@@ -109,13 +129,17 @@ function showAllGames() {
     deleteChildElements(gamesContainer);
 
     // add all games from the JSON data to the DOM
-
+    addGamesToPage(GAMES_JSON)
 }
 
 // select each button in the "Our Games" section
 const unfundedBtn = document.getElementById("unfunded-btn");
+unfundedBtn.addEventListener('click', filterUnfundedOnly)
 const fundedBtn = document.getElementById("funded-btn");
+fundedBtn.addEventListener('click', filterFundedOnly)
 const allBtn = document.getElementById("all-btn");
+allBtn.addEventListener('click', showAllGames);
+
 
 // add event listeners with the correct functions to each button
 
@@ -129,10 +153,15 @@ const allBtn = document.getElementById("all-btn");
 const descriptionContainer = document.getElementById("description-container");
 
 // use filter or reduce to count the number of unfunded games
-
+const numUnfunded = GAMES_JSON.filter(game => game.pledged < game.goal).length
+const numFunded = GAMES_JSON.length - numUnfunded
 
 // create a string that explains the number of unfunded games using the ternary operator
 
+const displayStr = `A total number of $${totalRaised.toLocaleString()} has been raised for ${numFunded} ${numFunded !== 1 ? "games" : "game"}. Currently ${numUnfunded} ${numUnfunded !== 1 ? "games remain" : "game remains"} unfunded. We need your help to fund these amazing games!`
+const descParag = document.createElement('p');
+descParag.textContent = displayStr;
+descriptionContainer.appendChild(descParag)
 
 // create a new DOM element containing the template string and append it to the description container
 
@@ -149,7 +178,13 @@ const sortedGames =  GAMES_JSON.sort( (item1, item2) => {
 });
 
 // use destructuring and the spread operator to grab the first and second games
-
+let [first, second]  = sortedGames;
 // create a new element to hold the name of the top pledge game, then append it to the correct element
+const firstGame = document.createElement('h3');
+firstGame.textContent = first.name;
+firstGameContainer.appendChild(firstGame)
+const secondGame = document.createElement('h3');
+secondGame.textContent = second.name;
+secondGameContainer.appendChild(secondGame)
 
 // do the same for the runner up item
